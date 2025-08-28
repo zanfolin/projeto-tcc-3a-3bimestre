d9b7de7eb8dcd4d850b5152461a541d0e2e6e430
Graziani V Zafolin <zanfolin@gmail.com>
2025-08-28 09:06:14 -0300

    [feat] Get User Data

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { ActivityIndicator, Alert, Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    let URL = "http://192.168.100.164:3000/api";
+    setLoading(true);
+    try {
+      const response = await fetch(`${URL}/user`);
+      if (!response.ok) {
+        throw new Error(`Erro de servidor: ${response.status}`);
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(error.message);
+    } finally {
+      setLoading(false)
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Projeto TCC 3a</Text>
+      <View>
+        <Text>Dados do Banco</Text>
+        {data &&
+          data.map((item) => {
+            return <Text>{item.username}</Text>;
+          })}
+        
+        {
+          loading ? <ActivityIndicator />
+                  : <Button title="Buscar Dados" onPress={fetchData} />
+        }
+        
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +48,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
