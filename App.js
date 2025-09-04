eb3d5e8971b36749f3d35e6a003d2b1e938ac29d
Graziani V Zafolin <zanfolin@gmail.com>
2025-09-04 08:55:37 -0300

    [feat] Add User

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { ActivityIndicator, Alert, Button, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const [username, setUsername] = useState("Usuario ");
+  const [email, setEmail] = useState("usuario@email.com");
+
   const fetchData = async () => {
-    let URL = "http://192.168.100.164:3000/api";
+    let URL = "http://192.168.100.109:3000/api";
     setLoading(true);
     try {
       const response = await fetch(`${URL}/user`);
@@ -20,25 +31,67 @@ export default function App() {
       console.log(error);
       Alert.alert(error.message);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   };
+
+  const postData = async () => {
+    let URL = "http://192.168.100.109:3000/api";
+    setLoading(true);
+    try {
+      const body = {
+        username: username,
+        email: email,
+      };
+      const header = {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(body),
+      };
+      const response = await fetch(`${URL}/user`, header);
+      if (response.status != 201) {
+        throw new Error("Erro ao inserir dados");
+      }
+      const result = await response.json();
+      Alert.alert("Usuário criado com código: " + result.userId);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro ao enviar os dados: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleAdd = async () => {
+    //desenvolver esse botão
+  }
 
   return (
     <View style={styles.container}>
       <Text>Projeto TCC 3a</Text>
+      <View>
+        <TextInput value={username} onChangeText={setUsername} />
+        <TextInput value={email} onChangeText={setEmail} />
+        <Button title="Adicionar" onPress={postData} />
+      </View>
+
       <View>
         <Text>Dados do Banco</Text>
         {data &&
           data.map((item) => {
             return <Text>{item.username}</Text>;
           })}
-        
-        {
-          loading ? <ActivityIndicator />
-                  : <Button title="Buscar Dados" onPress={fetchData} />
-        }
-        
+
+        {loading ? (
+          <ActivityIndicator />
+        ) : (
+          <>
+            <Button title="Buscar Dados" onPress={fetchData} />
+            <Button title="Limpar Dados" onPress={() => setData([])} />
+          </>
+        )}
       </View>
       <StatusBar style="auto" />
     </View>
